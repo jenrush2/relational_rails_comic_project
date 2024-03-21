95b9e169a59559f20368ffb97ace597a02e6f3a9
Jennifer Rush <jennifer.rush2@gmail.com>
2024-03-21 11:24:22 -0600

    creates appears before matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,12 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+RSpec::Matchers.define :appear_before do |later_content|
+  match do |earlier_content|
+    page.body.index(earlier_content) < page.body.index(later_content)
+  end
+end
+
+
 end
