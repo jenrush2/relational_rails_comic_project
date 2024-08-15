f8c9f2924ea44a8257a181a64a800cf66239ca67
Jennifer Rush <jennifer.rush2@gmail.com>
2024-08-15 11:30:27 -0600

    adds delete button to store show page

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ApplicationRecord
-    has_many :books
+    has_many :books, dependent: :destroy
 
     def self.sort_by_most_recent
         Store.order(created_at: :desc)
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   post '/stores/:store_id/books', to: 'store_books#create'
   get '/books/:book_id/edit', to: 'books#edit'
   patch '/books/:book_id', to: 'books#update'
+  delete '/stores/:store_id', to: 'stores#destroy'
   
   
   
