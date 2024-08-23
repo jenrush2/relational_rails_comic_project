0106d4b30a789f0bfc840b7e7d3824b665887283
Jennifer Rush <jennifer.rush2@gmail.com>
2024-08-23 14:14:36 -0600

    adds comments in store model for future joins work

--- app/models/store.rb
+++ app/models/store.rb
@@ -14,6 +14,9 @@ class Store < ApplicationRecord
     end
 
     def self.sort_by_number_of_books
+        #working on a joins method to complete this,
+        #but haven't actually done the joins lesson
+        #Store.joins(:books).sort_by(books.length :desc)
         Store.all.sort{|a,b| b.books.length <=> a.books.length}
     end
 
